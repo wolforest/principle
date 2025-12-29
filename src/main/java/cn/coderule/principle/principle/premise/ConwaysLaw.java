6fd272a6047c60000056806008007a8610e0889b
winglechen@aliyun.com <winglechen@gmail.com>
2025-12-29 15:24:54 +0800

    feature: add LawOfEntropy

--- src/main/java/cn/coderule/principle/principle/premise/ConwaysLaw.java
+++ src/main/java/cn/coderule/principle/principle/premise/ConwaysLaw.java
@@ -1,4 +1,10 @@
 package cn.coderule.principle.principle.premise;
 
+/**
+ * Conway's Law
+ * Any organization that designs a system
+ * will produce a design whose structure
+ * mirrors the organization’s communication structure.
+ */
 public @interface ConwaysLaw {
 }
---
+++ src/main/java/cn/coderule/principle/principle/premise/LawOfEntropy.java
@@ -0,0 +1,8 @@
+package cn.coderule.principle.principle.premise;
+
+/**
+ * The Law Of Entropy
+ * In an isolated system, entropy always increases over time.
+ */
+public @interface LawOfEntropy {
+}
